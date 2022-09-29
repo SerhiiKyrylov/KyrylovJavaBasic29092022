846541520237b777841e29bd85a431ddd43ec4cd
SerhiiKyrylov <kirillov@ua.fm>
2022-09-29 22:14:49 +0300

    changed code

--- src/KyrylovJavaBasic.java
+++ src/KyrylovJavaBasic.java
@@ -1,5 +1,6 @@
 public class KyrylovJavaBasic {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Goodbye JavaBasic course");
     }
 }
